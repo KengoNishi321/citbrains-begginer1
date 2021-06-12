941f95d341f2b2c863fffa2eb7edffee41d52a0a
KengoNishi321 <nishi@DESKTOP-FAI6HF9.localdomain>
2021-06-12 14:49:45 +0900

    mondai

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int a = 3, aa = 4, aaa = 5, aaaa = 6, aaaaa, nijuuhachi;
+
+  int main(void) {
+  
+   int b=5,c=9, n=11, m=19, x;
+
+     x = b >= m;
+     x += 2 == n - m + b*2;
+     x -= 1 != b*3 - m + b - 3;
+
+     printf("%d\n",x);
+  return 0;
+}
+
